0753290bd6f2356e36c7347eb930fa787ab29655
Diana <diana.vazquezro3@gmail.com>
2024-02-05 15:38:59 -0600

    feat: :art: Maquetado de los cards de noticias
    close #5

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -7,8 +7,41 @@
 
 <?php $this->section('content') ?>
 
-<h1>Busca entre tus sitios favoritos</h1>
+<h1 class="title">Busca entre tus sitios favoritos</h1>
 
 <?= view('components/searchbar')?>
+
+
+<div class="cards--container">
+    <?php
+        for ($i = 1; $i <= 5; $i++) {
+    ?>
+        <div class="card">
+            <div class="card__image">
+                <img src="/assets/images/flores.jpg">
+            </div>
+            <div class="card__info">
+                <h1 class="card__info__title">Este es el título de la noticia</h1>
+                <div class="card__info__categorie">
+                <?php
+                    for ($j = 1; $j <= 2; $j++) {
+                ?>
+                     <p class="card__info__categorie__title">Categoría</p>   
+                 <?php
+                    }
+                ?>
+                </div>
+                <p class="card__info__date content--size">12/06/24</p>
+                <p class="card__info__description content--size">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore minim veniam, minim veniam, quis...
+                </p>
+                <a class="card__info__link content--size" href="#">-> Leer Más</a>
+            </div>
+        </div>
+    <?php
+        }
+    ?>
+</div>
 
 <?php $this->endSection() ?>
