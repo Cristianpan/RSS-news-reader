3576ab59f5513b85f25fa1941374b9098f87bd8e
Erick Arzat <erick_arzat11@homtail.es>
2024-02-07 21:52:25 -0600

    Implementacion del Navbar

---
+++ app/Views/components/navbar.php
@@ -0,0 +1,22 @@
+<header class="header">
+
+    <a class="logo" href="#">
+        <img class="logo__imagen" src="/assets/images/img2.svg" alt="logo">
+    </a>
+
+    <input type="checkbox" name="" id="check">
+    <label for="check" class="checkbtn">
+        <img src="/assets/images/list.svg" alt="menu" class="checkbtn__img">
+    </label>
+
+    <nav class="nav">
+        <div class="nav__links">
+            <a class="nav__link nav__link--active" href="#">Inicio</a>
+            <a class="nav__link" href="#">Registra Feeds</a>
+        </div>
+    </nav>
+    
+    <script src="<?= base_url()?>/assets/js/navbar.min.js"></script>
+    
+</header>
+
