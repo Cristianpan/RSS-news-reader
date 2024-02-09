583289697023ffd12e126073d447b364161dff9f
JosePuc <pucjl07@gmail.com>
2024-02-08 21:22:22 -0600

    feat: :art: Maquetado de la barra de busqueda y filtro
    close #4

---
+++ app/Views/components/searchbar.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="search-bar">
+        <form action="#" method="get" class="search-bar__form">
+            <input type="text" placeholder="Ingresa el título, nombre de la página o categoría" class="search-bar__input">
+            <button type="submit" class="search-bar__button">
+                <img src="/assets/images/search-icon.svg" alt="">
+            </button>
+        </form>
+    </div>
+    <div class="news-filter">
+            <label for="" class="news-filter__label">Ordenar por:</label>
+            <select name="news-filter__select" id="news-filter__select" class="news-filter__select">
+                <option value="fecha">Fecha</option>
+                <option value="titulo">Título</option>
+                <option value="categoria">Categoría</option>
+            </select>
+            <div class="news-filter__arrow">
+                <span class="material-symbols-outlined">
+                    arrow_drop_down
+                </span>
+            </div>
+        </div>
+</div>
