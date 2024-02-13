859985654105a199be98b78d84e63410e0db065f
Cristian Pan <a16003566@alumnos.uady.mx>
2024-02-13 01:05:26 -0600

    fix: :art: Update websites controller and route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Controllers\CtrlWebsites;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/websites', 'Home::websitesView');
+
+$routes->get('/websites', [CtrlWebsites::class, 'index'], ['as' => 'websites']);
